a3a97924f11fed0a37325de85bba6375830d7bdd
Back1ng <anatolyu128@gmail.com>
2020-07-15 14:02:06 +0500

    wip design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,39 +1,33 @@
 @extends('welcome')
 
 @section('content')
-    <div class="bg-blue-400 p-4 mt-5 h-auto shadow rounded">
-        <p class="flex justify-center text-white" style="font-size: 14pt;">Create new section</p>
+    <div class="bg-blue-400 w-full lg:w-2/6 float-left shadow rounded my-6 p-6">
+        <p class="text-white text-center" style="font-size: 14pt;">Create new section</p>
         @error('name')
-            <div class="alert alert-danger flex justify-center w-25 m-auto">{{ $message }}</div>
+        <div class="alert alert-danger m-auto">{{ $message }}</div>
         @enderror
-        <form action="/section" method="post" class="flex justify-center mt-2">
+        <form action="/section" method="post" class="mt-2 flex justify-center">
             @csrf
-
             <input type="text" class="p-2 ml-2 shadow rounded" name='name' placeholder="Section Name">
-
-            <input type="submit" class="p-2 w-1/12 ml-2 shadow rounded bg-blue-400 text-white" value="Create">
-
+            <input type="submit" class="p-2 ml-2 shadow rounded bg-blue-600 text-white" value="Create">
         </form>
+    </div>
+    <div class="d-block mx-auto">
+        <div class="bg-blue-400 w-full lg:w-7/12 my-6 p-6 shadow rounded float-right ">
+            <p class="text-center text-white" style="font-size: 14pt;">Select your section</p>
+            <div class="flex flex-wrap text-center">
+                @foreach ($sections as $section)
+                    <div class="bg-blue-200 shadow rounded p-6 m-2">
+                        <form method="post" action="/section/{{ $section->id }}">
+                            <a class="mr-6" href="/section/{{ $section->id }}" style="font-size: 14pt;">{{ $section->name }}</a>
 
-        <br>
-        <p class="flex justify-center text-white" style="font-size: 14pt;">Select your section</p>
-
-        <div class="flex flex-wrap text-center">
-        @foreach ($sections as $section)
-            <div class="bg-blue-200 shadow rounded p-3 m-2">
-                {{ $section->name }}
-                <form method="post" action="/section/{{ $section->id }}">
-                    @method('DELETE')
-                    @csrf
-
-                    <input class="p-1 shadow rounded bg-red-500 text-white" type="submit" value="Delete">
-
-                    <a class="p-1 shadow rounded bg-orange-500 text-white" href="/section/{{ $section->id }}">Show</a>
-                </form>
+                            @method('DELETE')
+                            @csrf
+                            <input class="p-1 shadow rounded bg-red-500 text-white" type="submit" value="Delete">
+                        </form>
+                    </div>
+                @endforeach
             </div>
-
-        @endforeach
         </div>
-
     </div>
 @endsection
